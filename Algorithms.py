57d6bcba0a42be173aa929580763863f9ab1f584
ReemMahm0ud <53402760+ReemMahm0ud@users.noreply.github.com>
2019-07-29 20:03:47 +0200

    Update Algorithms.py

--- Algorithms.py
+++ Algorithms.py
@@ -1,5 +1,5 @@
 from node import Board
-
+rom collections import deque
 
 def dfs(initialState):
     count = 0
@@ -21,3 +21,29 @@ def dfs(initialState):
                 frontier.append(neighbor)
 
     return False, state
+
+
+def bfs (initialState):
+    step = 0
+    d = 0
+    frontier = deque([initialState])
+    explored = set()
+    explored.add(initialState.id)
+    
+    while frontier:
+        state = frontier.popleft()
+        
+        step= step+1
+        print(step)
+        state.printState()
+        
+        
+        if state.compare():
+            return True,[state.getpath(),d]
+        
+        for neighbor in state.getNeighbors():
+            if not(neighbor.id  in explored):
+                frontier.append(neighbor)
+                explored.add(neighbor.id)
+                if neighbor.g > d:
+                    d = neighbor.g
