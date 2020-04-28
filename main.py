1ef8bcce0716c9a504097feac9546777bc9b9c44
omar gamily <ogamily97@gmail.com>
2020-04-28 03:01:14 +0200

    change use time instead of clock

--- main.py
+++ main.py
@@ -1,47 +1,41 @@
 from node import Board
-from time import clock
 from Algorithms import dfs
 from Algorithms import bfs
 from Algorithms import aStar
+import time
 
 if __name__ == "__main__":
     state = Board([[1, 2, 3], [4, 5, 0], [6, 7, 8]])
 
     userinput = input("[1]dfs    [2]bfs  [3]A*(Manhattan Distance)  [4]A*(Euclidean Distance) :-->")
 
-    try:
-        val = int(userinput)
-        print("....\n")
-    except ValueError:
-        print("try again")
-
     if userinput == '1':
-        before = clock()
+        before = time.time()
         x = dfs(state)
-        after = clock()
+        after = time.time()
         print('process', x[0])
         print('tree depth', x[2])
         print('time took ', after - before, ' seconds')
     elif userinput == '2':
-        before = clock()
+        before = time.time()
         x = bfs(state)
-        after = clock()
+        after = time.time()
         print('process', x[0])
         print('tree depth', x[2])
         print('time took ', after - before, ' seconds')
     elif userinput == '3':
-        before = clock()
+        before = time.time()
         state = Board([[1, 2, 3], [4, 5, 0], [6, 7, 8]], algo='a* manhattan')
         x = aStar(state)
-        after = clock()
+        after = time.time()
         print('process', x[0])
         print('tree depth', x[2])
         print('time took ', after - before, ' seconds')
     elif userinput == '4':
-        before = clock()
+        before = time.time()
         state = Board([[1, 2, 3], [4, 5, 0], [6, 7, 8]], algo='a* eculidean')
         x = aStar(state)
-        after = clock()
+        after = time.time()
         print('process', x[0])
         print('tree depth', x[2])
         print('time took ', after - before, ' seconds')
